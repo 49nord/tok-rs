ea36b553a8c73b661e4c7dca8687842773844d56
Reiner <reiner.dolp@49nord.de>
2018-01-10 15:25:33 +0100

    make serde traits and hash trait optional, use 1-tuple instead of struct { data }

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,39 @@
 #[cfg(any(feature = "serialize", feature = "deserialize"))]
 extern crate serde;
-#[cfg(any(feature = "serialize", feature = "deserialize"))]
-#[macro_use]
-extern crate serde_derive;
 
 extern crate constant_time_eq;
 extern crate rand;
 
+#[cfg(feature = "serialize")]
+use serde::Serializer;
+
+#[cfg(feature = "deserialize")]
+use serde::Deserializer;
+
 use std::cmp;
+use std::hash::{Hasher, Hash};
 
 use self::rand::Rng;
 use self::constant_time_eq::constant_time_eq;
 
-#[derive(Clone, Debug, Hash)]
-#[cfg_attr(feature = "serialize", derive(Serialize))]
-#[cfg_attr(feature = "deserialize", derive(Deserialize))]
-pub struct Token<S> {
-    data: S,
-}
+#[derive(Clone, Debug)]
+pub struct Token<S>(S);
 
 impl<S: rand::Rand> Token<S> {
     pub unsafe fn create(data: S) -> Token<S> {
-        Token { data }
+        Token(data)
     }
 
     pub fn generate() -> Token<S> {
         let mut rng = rand::thread_rng();
 
-        Token { data: rng.gen() }
+        Token(rng.gen())
     }
 }
 
 impl<S: AsRef<[u8]>> PartialEq for Token<S> {
     fn eq(&self, other: &Self) -> bool {
-        constant_time_eq(self.data.as_ref(), other.data.as_ref())
+        constant_time_eq(self.0.as_ref(), other.0.as_ref())
     }
 }
 
@@ -41,15 +41,15 @@ impl<S: AsRef<[u8]>> Eq for Token<S> {}
 
 impl<S: AsRef<[u8]>> PartialOrd for Token<S> {
     fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
-        let len_self = self.data.as_ref().len();
-        let len_other = other.data.as_ref().len();
+        let len_self = self.0.as_ref().len();
+        let len_other = other.0.as_ref().len();
 
         if len_self == len_other {
             Some(
-                self.data
+                self.0
                     .as_ref()
                     .iter()
-                    .zip(other.data.as_ref().iter())
+                    .zip(other.0.as_ref().iter())
                     .filter_map(|(s, o)| s.partial_cmp(o))
                     .skip_while(|&ord| ord == cmp::Ordering::Equal)
                     .next()
@@ -62,11 +62,40 @@ impl<S: AsRef<[u8]>> PartialOrd for Token<S> {
     }
 }
 
+
+#[cfg(feature = "serialize")]
+impl<T: serde::Serialize> serde::Serialize for Token<T> {
+    #[inline]
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        self.0.serialize(serializer)
+    }
+}
+
+#[cfg(feature = "deserialize")]
+impl<'de, T: serde::Deserialize<'de>> serde::Deserialize<'de> for Token<T> {
+    #[inline]
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        T::deserialize(deserializer).map(Token)
+    }
+}
+
+impl<T: Hash> Hash for Token<T> {
+    #[inline]
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.0.hash(state);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::collections::hash_map::DefaultHasher;
-    use std::hash::{Hasher, Hash};
 
     #[test]
     fn test_token_eq() {
